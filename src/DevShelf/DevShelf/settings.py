23ed624fde7a57a48cbb8fdf1c2bd82ee3007eb3
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-01 12:43:15 +0530

    .

--- src/DevShelf/DevShelf/settings.py
+++ src/DevShelf/DevShelf/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: Keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)e_sfwyyv3tf8b^09ow$5*yr^^g=13i9qyg(s83+u#*f5fa671'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: Don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []

a7b62a67003c8548ee4a28164ccda2896e677333
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-08 16:39:13 +0530

    Updated LoginPage

--- BackEnd/devshelf/user/static/LoginPage,Script.js
+++ BackEnd/devshelf/user/static/LoginPage,Script.js
@@ -2,25 +2,25 @@ console.log("Welcome to the Login Page");
 
 
 
-document.addEventListener("keypress", function(event) {
-    const messageElement = document.querySelector(".message");
-    if(event.keyCode=116)
+document.addEventListener("keypress", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
     messageElement.style.display = "none";
-  });
+});
 
-  
-document.addEventListener("keydown", function(event) {
-    const messageElement = document.querySelector(".message");
-    if(event.keyCode=116)
-    messageElement.style.display = "none";
-  });
 
-  
-document.addEventListener("keyup", function(event) {
-    const messageElement = document.querySelector(".message");
-    if(event.keyCode=116)
+document.addEventListener("keydown", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
     messageElement.style.display = "none";
-  });
+});
+
+
+document.addEventListener("keyup", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
+    messageElement.style.display = "none";
+});
 
 
 

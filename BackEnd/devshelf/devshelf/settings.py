d2a3b7a95f1ab6dd1d4265c40d70d7f5c6afa9a6
neel642 <neels.kowshik@gmail.com>
2024-07-09 13:17:07 +0530

    typo fixed

--- BackEnd/devshelf/devshelf/settings.py
+++ BackEnd/devshelf/devshelf/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'devshelf',
-        'USER': 'postgress',
+        'USER': 'postgres',
         'PASSWORD': 'Abc@123',
         'HOST': '127.0.0.1',
         'PORT': '5432',

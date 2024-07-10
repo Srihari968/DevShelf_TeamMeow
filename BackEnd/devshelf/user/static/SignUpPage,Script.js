7bd66e6600f295b4ecf4f5cdc52e27a233070667
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-11 00:31:47 +0530

    Added JS to SignUp Page

---
+++ BackEnd/devshelf/user/static/SignUpPage,Script.js
@@ -0,0 +1,23 @@
+console.log("Welcome to the Sign Up Page");
+
+
+
+document.addEventListener("keypress", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
+    messageElement.style.display = "none";
+});
+
+
+document.addEventListener("keydown", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
+    messageElement.style.display = "none";
+});
+
+
+document.addEventListener("keyup", function (event) {
+  const messageElement = document.querySelector(".message");
+  if (event.keyCode = 116)
+    messageElement.style.display = "none";
+});

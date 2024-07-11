862e69e02e9e9ac700cad100e8caaa917df068d9
YashX777 <yashas060205@gmail.com>
2024-07-12 01:21:42 +0530

    ALignment changes are done

--- BackEnd/devshelf/library/static/search.js
+++ BackEnd/devshelf/library/static/search.js
@@ -4,21 +4,36 @@ document.addEventListener('DOMContentLoaded', function() {
     let ans=false;
 
     boxes.forEach((box) => {
-        
-        
-
-        
             box.addEventListener("click", () => {
                 console.log("info clicked");
                 box.classList.toggle("flip");
                 
             })
-        
-    
-        
-        
-        
+
     });
+    let buttons = document.querySelectorAll(".box-btn");
+    
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
+            event.stopPropagation();
+            let dialog=document.querySelector(".dialog");
+            dialog.style.display="block";
+        
+
+            let close = document.querySelector(".close");
+            close.addEventListener("click", () => {
+                dialog.style.display="none";
+            })
+            let cancel = document.querySelector("#btn1");
+            cancel.addEventListener("click", () => {
+                dialog.style.display="none";
+            })
+
+
+        })
+    })
+   
 
 });
 

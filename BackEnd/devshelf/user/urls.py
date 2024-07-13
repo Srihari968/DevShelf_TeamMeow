0e501bb5d348efdf662c02952f1cce1508d9350d
neel642 <neels.kowshik@gmail.com>
2024-07-14 00:53:39 +0530

    reset password

--- BackEnd/devshelf/user/urls.py
+++ BackEnd/devshelf/user/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from .views import RegisterView
 from .views import login, signup
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('signup/',signup , name='signup'),
     path('login/', login, name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete//', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

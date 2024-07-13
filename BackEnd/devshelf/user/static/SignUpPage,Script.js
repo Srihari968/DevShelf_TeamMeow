e4c06f7ac055f9196e55adf5b7f0b38c1ae75df6
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-14 02:29:16 +0530

    Modified JS for Login and SignUp Pages

--- BackEnd/devshelf/user/static/SignUpPage,Script.js
+++ BackEnd/devshelf/user/static/SignUpPage,Script.js
@@ -4,20 +4,20 @@ console.log("Welcome to the Sign Up Page");
 
 document.addEventListener("keypress", function (event) {
   const messageElement = document.querySelector(".message");
-  if (event.keyCode = 116)
+  if (event.key== "F5" || (event.key.ctrlKey==true && event.key=="R"))
     messageElement.style.display = "none";
 });
 
 
 document.addEventListener("keydown", function (event) {
   const messageElement = document.querySelector(".message");
-  if (event.keyCode = 116)
+  if (event.key == "F5"  || (event.key.ctrlKey==true && event.key=="R"))
     messageElement.style.display = "none";
 });
 
 
 document.addEventListener("keyup", function (event) {
   const messageElement = document.querySelector(".message");
-  if (event.keyCode = 116)
+  if (event.key == "F5"  || (event.key.ctrlKey==true && event.key=="R"))
     messageElement.style.display = "none";
 });

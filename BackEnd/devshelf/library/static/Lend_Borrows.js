c79a04c7ee65dd84edb8976844fc4e856d5906ba
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-14 00:42:58 +0530

    Not that important

--- BackEnd/devshelf/library/static/Lend_Borrows.js
+++ BackEnd/devshelf/library/static/Lend_Borrows.js
@@ -1,4 +1,4 @@
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
+$(window).on("load resize ", function () {
+  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+  $('.tbl-header').css({ 'padding-right': scrollWidth });
+}).resize();

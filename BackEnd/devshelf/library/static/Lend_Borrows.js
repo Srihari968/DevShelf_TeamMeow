848af62b3875d34c9cfedf62ded0ed6b01956549
DhanushHN10 <dhanush.hn01@gmail.com>
2024-07-14 23:44:29 +0530

    Added Dynamic Sidebar to LendBorrows and ReceiveBooks Pages

--- BackEnd/devshelf/library/static/Lend_Borrows.js
+++ BackEnd/devshelf/library/static/Lend_Borrows.js
@@ -2,3 +2,17 @@ $(window).on("load resize ", function () {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({ 'padding-right': scrollWidth });
 }).resize();
+
+//  const Menu = document.querySelector(".Menu");
+// $('Menu').click(function(){
+//    $(this).toggleClass("click");
+// });
+
+// Menu.addEventListener("click",() =>{
+//   Menu.toggleClass("click");
+// });
+
+$('.Menu').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
+});
